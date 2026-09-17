a711cc153152696024c341a0dd80473870e00317
hoangtuan-vlu <tuanvo.04032006@gmail.com>
2026-09-17 16:30:46 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
-function login(username, password) {
-    if (username === "admin" && password === "123") {
+function checkLogin(username, password) {
+    if (username === 'admin' && password === '123') {
         return true;
     }
-
     return false;
 }
 
-
+// Xuất hàm để sử dụng cho Jest (nếu dùng môi trường Node.js)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = checkLogin;
+}
